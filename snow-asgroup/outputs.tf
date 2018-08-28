8442732e0c46f6a729a2cf335eccbfa16a6e0c14
Brian Muhlenkamp <29447475+moskey71@users.noreply.github.com>
2018-08-28 15:12:30 -0400

    fix output file

--- snow-asgroup/outputs.tf
+++ snow-asgroup/outputs.tf
@@ -1,20 +1,15 @@
-#output "URL" {
-#  value = "https://${aws_acm_certificate.cert.domain_name}/guacamole"
-#}
 output "snowasg_ScaleDownScheduledAction" {
 value = "${aws_cloudformation_stack.snowasg.outputs.ScaleDownScheduledAction}"
 }
-#
-#output "rdcb_instanceip" {
-# value = "${lookup(data.aws_cloudformation_stack.rdcb.outputs, "RdcbEc2InstanceIp", "")}"
-#}
-#
-#output "rdcb_instanceid" {
-# value = "${lookup(data.aws_cloudformation_stack.rdcb.outputs, "RdcbEc2InstanceId", "")}"
-#}
-
-# "outputs.ScaleDownScheduledAction": "test-Scale-1B0WTC7DYV301",
-# "outputs.ScaleUpScheduledAction": "test-Scale-13TL5ZNYEM1HJ",
-# "outputs.ServiceNowAutoscalingGroupId": "test-snow-autoscaling-bfm-ServiceNowAutoscalingGroup-1L578XTSVS3CC",
-# "outputs.ServiceNowLaunchConfigId": "test-snow-autoscaling-bfm-ServiceNowLaunchConfig-99HEFULWSU0W",
-# "outputs.ServiceNowLaunchConfigLogGroupName": "/aws/ec2/lx/test-snow-autoscaling-bfm",
+output "snowasg_ScaleUpScheduledAction" {
+value = "${aws_cloudformation_stack.snowasg.outputs.ScaleUpScheduledAction}"
+}
+output "snowasg_ServiceNowAutoscalingGroupId" {
+value = "${aws_cloudformation_stack.snowasg.outputs.ServiceNowAutoscalingGroupId}"
+}
+output "snowasg_ServiceNowLaunchConfigId" {
+value = "${aws_cloudformation_stack.snowasg.outputs.ServiceNowLaunchConfigId}"
+}
+output "snowasg_ServiceNowLaunchConfigLogGroupName" {
+value = "${aws_cloudformation_stack.snowasg.outputs.ServiceNowLaunchConfigLogGroupName}"
+}

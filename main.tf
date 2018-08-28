31a14c6ddbcd2a5cbc8a526fe6f530be1fd7b311
Brian Muhlenkamp <29447475+moskey71@users.noreply.github.com>
2018-08-28 14:43:38 -0400

    added outputs for CFn stack deployment

--- main.tf
+++ main.tf
@@ -58,3 +58,19 @@ module "snow-asgroup" {
   "s3bucket" = "${var.s3bucket}"
   "local-exec-profile" = "${var.local-exec-profile}" 
   }
+
+output "snowasg_ScaleDownScheduledAction" {
+value = "${module.snow-asgroup.snowasg_ScaleDownScheduledAction}"
+}
+output "snowasg_ScaleUpScheduledAction" {
+value = "${module.snow-asgroup.snowasg_ScaleUpScheduledAction}"
+}
+output "snowasg_ServiceNowAutoscalingGroupId" {
+value = "${module.snow-asgroup.snowasg_ServiceNowAutoscalingGroupId}"
+}
+output "snowasg_ServiceNowLaunchConfigId" {
+value = "${module.snow-asgroup.snowasg_ServiceNowLaunchConfigId}"
+}
+output "snowasg_ServiceNowLaunchConfigLogGroupName" {
+value = "${module.snow-asgroup.snowasg_ServiceNowLaunchConfigLogGroupName}"
+}

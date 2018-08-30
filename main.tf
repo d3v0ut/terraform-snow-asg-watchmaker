538f5f4079e2909a4f8d0575ddb20996a7cd4b73
Brian Muhlenkamp <29447475+moskey71@users.noreply.github.com>
2018-08-30 12:20:35 -0400

    add initial conditionals

--- main.tf
+++ main.tf
@@ -60,6 +60,7 @@ module "snow-asgroup" {
   
   }
 resource "aws_route53_record" "lb_pub_dns" {
+  count = "${var.commercial == true ? 1 : 0}"
   zone_id = "${var.public_dnszone_id}"
   name    = "${var.dns_name}"
   type    = "A"
@@ -70,6 +71,7 @@ resource "aws_route53_record" "lb_pub_dns" {
   }
 }
 resource "aws_acm_certificate" "cert" {
+  count = "${var.commercial == true ? 1 : 0}"
   domain_name       = "${aws_route53_record.lb_pub_dns.fqdn}"
   validation_method = "DNS"
 
@@ -79,6 +81,7 @@ resource "aws_acm_certificate" "cert" {
   }
 }
 resource "aws_route53_record" "cert_validation" {
+  count = "${var.commercial == true ? 1 : 0}"
   name    = "${aws_acm_certificate.cert.domain_validation_options.0.resource_record_name}"
   type    = "${aws_acm_certificate.cert.domain_validation_options.0.resource_record_type}"
   zone_id = "${var.public_dnszone_id}"
@@ -86,6 +89,7 @@ resource "aws_route53_record" "cert_validation" {
   ttl     = 60
 }
 resource "aws_acm_certificate_validation" "cert" {
+  count = "${var.commercial == true ? 1 : 0}"
   certificate_arn         = "${aws_acm_certificate.cert.arn}"
   validation_record_fqdns = ["${aws_route53_record.cert_validation.fqdn}"]
 }
@@ -108,6 +112,7 @@ resource "aws_lb_target_group" "alb_tg" {
   deregistration_delay = 0
 }
 resource "aws_lb_listener" "alb_listener" {
+  count = "${var.commercial == true ? 1 : 0}"
   load_balancer_arn = "${aws_lb.alb.arn}"
   port              = "443"
   protocol          = "HTTPS"
@@ -118,6 +123,7 @@ resource "aws_lb_listener" "alb_listener" {
     type             = "forward"
   }
 }
+
 resource "aws_security_group" "lb-sg1" {
   name_prefix        = "${var.stackname}-lb-sg1"
   description = "Security group for accessing SNow via the Internet"
